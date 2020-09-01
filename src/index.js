f9ca20c8fc5fb19517883f8ace53ae79501edbf2
guliver1 <gahal1599@gmail.com>
2020-09-01 14:54:26 +0300

    Added game board initialization and components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import MineSweeper from './MineSweeper';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MineSweeper />
   </React.StrictMode>,
   document.getElementById('root')
 );
