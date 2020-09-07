466e387ecb1f18451159424cf2869b571093d783
guliver1 <gahal1599@gmail.com>
2020-09-07 11:24:31 +0300

    Github pages build

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux';
-import boardReducer from './board';
-
-// Combine different reducers here
-const rootReducer = combineReducers({boardReducer});
-
-export default rootReducer;
